9329d5c156805061d18e3cef7d9b8cf91e79b0c8
Vivatist <anvabo@gmail.com>
2023-10-11 12:34:44 +0300

    Прото-публикации в канал

--- app.py
+++ app.py
@@ -1,25 +1,8 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 
 import constats
 
-from telegram import ForceReply, Update
+from telegram import ForceReply, Update, Bot
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -28,18 +11,22 @@ from telegram.ext import (
     filters,
 )
 
-# Enable logging
+TOKEN = constats.TOKEN_BOT
+CHAT_ID = constats.CHANNEL
+
+bot = Bot(token=TOKEN)
+
+# Старт логирования
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+# /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -49,29 +36,37 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
+# /help
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
 
+# OTHER
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
+    await post_to_channel(CHAT_ID, update.message.text)
+
+
+# Отправка текста  в канал от имени бота
+async def post_to_channel(chat_id, text):
+    print("Публикуем пост: ", text)
+    await bot.send_message(chat_id=chat_id, text=text)
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(constats.TOKEN).build()
+    """Стартуем бота"""
+    application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
+    # Команды
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
+    # Если не команда - значит сообщение
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # Run the bot until the user presses Ctrl-C
+    # Запускаем бота пока не Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
